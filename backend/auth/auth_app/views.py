8c5a7e783b873f6094d0656c694d6eedad061517
Lyess Garfi <lgarfi@bess-f2r4s11.clusters.42paris.fr>
2025-01-04 21:06:19 +0100

    ca reconnait bien l'authentification mtn faire pareil avec une api rest

--- backend/auth/auth_app/views.py
+++ backend/auth/auth_app/views.py
@@ -4,10 +4,20 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 def index(request):
-    print("request = ", request.body)
-    
+    if request.method == "POST":
+        print(request.body)
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+        print("name = ", username, "| password = ", username, password)
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            print("je ne reconnais pas cette user")
+        else:
+            print("je reconnais cette user")
+            
     return render(request, "auth_app/index.html")
 
 def result(request, user): #user est un parametre ajouter a cette view depuis l'url
