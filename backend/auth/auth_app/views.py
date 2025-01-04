b02aed0c921297822661b2ab2ab06cce23a78bc8
Lyess Garfi <lgarfi@bess-f2r4s11.clusters.42paris.fr>
2025-01-04 18:55:16 +0100

    ajout: d'un template pour afficher une page login mot de passe qui me servira a savoir si l'user est sur la bdd (y'en a deja un sur la bdd comme ca je peux savoir si ca marche ) chose a faire: 	reussir avec faire une autre page d'inscription et de l'enregistrer dans la bdd 	dans un monde parfait, faire en sorte qu'il y est un boutton pour allez directement sur la page d'inscription

--- backend/auth/auth_app/views.py
+++ backend/auth/auth_app/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, ici auth")
+    print("request = ", request.body)
+    
+    return render(request, "auth_app/index.html")
+
+def result(request, user): #user est un parametre ajouter a cette view depuis l'url
+    return HttpResponse("your looking at user %d." % (user * 5))
