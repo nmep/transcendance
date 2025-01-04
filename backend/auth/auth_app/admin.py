acfa33a62f9b4bca1a04f99af4e6f9e91f7106d9
Lyess Garfi <lgarfi@paul-f4Br3s2.clusters.42paris.fr>
2025-01-04 16:18:01 +0100

    ajout: 	Super user django 	de l'app sur la page admin du serv django pour pouvoir creer des objets du modele sans avoir a passer par le shell

--- backend/auth/auth_app/admin.py
+++ backend/auth/auth_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import authConf
+
+admin.site.register(authConf) # sert a dire a django que l'app auth peut etre modifie sur le site admin
