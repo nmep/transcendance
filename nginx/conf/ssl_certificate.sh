c53e61078244e1271adb66c01bdbdbe156c31a18
Lyess Garfi <lgarfi@bess-f2r1s11.clusters.42paris.fr>
2025-01-08 21:49:02 +0100

    le proxy est bon mais faudra penser a faire une location par api

---
+++ nginx/conf/ssl_certificate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p /etc/nginx/ssl/;
+
+openssl req -new -newkey rsa:4096 -nodes \
+    -keyout /etc/nginx/ssl/www.Mickeytracendence.com.key -out /etc/nginx/ssl/www.Mickeytracendence.com.csr \
+    -subj "/C=FR/ST=42/L=Paris/O=Dis/CN=www.Mickeytracendence.com"
+
+openssl x509 -req -days 365 \
+    -in /etc/nginx/ssl/www.Mickeytracendence.com.csr \
+    -signkey /etc/nginx/ssl/www.Mickeytracendence.com.key \
+    -out /etc/nginx/ssl/www.Mickeytracendence.com.crt
+
+exec nginx -g "daemon off;"
