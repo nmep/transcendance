ac6ef9e372258566f02dbe8200ff9d0d46fc985d
Lyess Garfi <lgarfi@made-f0Ar13s2.clusters.42paris.fr>
2025-01-20 19:46:47 +0100

    vault si je veux faire une commande je dois avoir le root token et l'idee c'est de faire un token avec le root token, autre soucis l'unseal du serveur necessite de rentre les token Shamir et ca doit etre fait de facon automatique voir les commandes et tout le tralalal oublie pas couincouintoken

--- security/vault/init.sh
+++ security/vault/init.sh
@@ -4,18 +4,12 @@ apk add openssl
 
 mkdir -p /vault/tls
 
-echo 1
-
 openssl req -nodes -newkey rsa:4096 -keyout /vault/tls/vault.key -out /vault/tls/vault.csr \
             -subj "/CN=FR/ST=OpenSSL/L=Paris/O=Dis/CN=127.0.0.1"
-
-echo 2
 
 openssl x509 -req -days 356 \
             -in /vault/tls/vault.csr \
             -signkey /vault/tls/vault.key \
             -out /vault/tls/vault.crt
-
-echo 3
 
 vault server -config /vault/config/config.hcl
