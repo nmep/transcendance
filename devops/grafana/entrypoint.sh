2ae3215f2f7c8b14fad354c78281cc12a3e8d7fe
dwayenbo <dwayenbo@student.42.fr>
2025-02-05 22:45:14 +0100

    allez plus qu'a verifier que ca fonctionne tjr

--- devops/grafana/entrypoint.sh
+++ devops/grafana/entrypoint.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 while [[ $(curl -k -s -f https://vault:8200/v1/sys/seal-status | jq -r .sealed) = "true" ]]; do
-    echo waiting for vault...
+    echo Waiting for vault to unseal...
     sleep 2
 done
 
-sleep 2
-
-export GF_SECURITY_ADMIN_PASSWORD=$(curl -s -k \
+while [ -z "$GF_SECURITY_ADMIN_PASSWORD" ] || [ "$GF_SECURITY_ADMIN_PASSWORD" = "null" ] ; do
+	sleep 2
+	export GF_SECURITY_ADMIN_PASSWORD=$(curl -s -k \
 									--header "X-Vault-Token:$VAULT_RTOKEN" \
 									https://vault:8200/v1/secret/grafana \
 									| jq -r .data.GF_SECURITY_ADMIN_PASSWORD)
-
-export GF_SECURITY_ADMIN_USER=$(curl -s -k \
+done
+while [ -z "$GF_SECURITY_ADMIN_USER" ] || [ "$GF_SECURITY_ADMIN_USER" = "null" ] ; do
+	sleep 2
+	export GF_SECURITY_ADMIN_USER=$(curl -s -k \
 									--header "X-Vault-Token:$VAULT_RTOKEN" \
 									https://vault:8200/v1/secret/grafana \
 									| jq -r .data.GF_SECURITY_ADMIN_USER)
+done
 
-echo niqueprout ${GF_SECURITY_ADMIN_PASSWORD} $GF_SECURITY_ADMIN_USER
 unset VAULT_RTOKEN
 
 exec "$@"
