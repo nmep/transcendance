d457be9869e089fede3b2e6a4dfe90f2a4c275c5
Lyess Garfi <lgarfi@made-f0Ar1s2.clusters.42paris.fr>
2025-03-04 20:17:25 +0100

    je rentre

--- backend/db/srcs/db_project/settings.py
+++ backend/db/srcs/db_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-q-v#jf3eh!wvmj7+=+&b5!!0=dqw9t=3j^qs+p=9p^oi_=05p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
