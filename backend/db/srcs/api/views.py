191fce9b3747ac72af53bb39b52350e9a1b2cd52
Lyess Garfi <lgarfi@made-f0Ar1s4.clusters.42paris.fr>
2025-03-05 20:56:18 +0100

    api db finit

--- backend/db/srcs/api/views.py
+++ backend/db/srcs/api/views.py
@@ -2,18 +2,16 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .serializers import dbSerializer
 # from rest_framework.request import Request
 
 class DB_API(APIView):
 	def get(self, request):
 		print("1")
 		if request.user:
-			print(f"r user = {request.user}")
 			user = User.objects.get(username=request.user)
 			if (user == None):
-				print(f"aucun user n'a été reconnue")
 				return Response({"message": "not ok"})
-			print(f"{user.__dict__}")
-		else:
-			print("user non reconnue")
-		return Response({"message": "ok"})
+			serializer = dbSerializer(user)
+			return Response(serializer.data)
+		return Response({"message": "User non reconnue"})
