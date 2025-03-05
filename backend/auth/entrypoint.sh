593ad1d1411771a15c2ff90414e4b0fb298eb51c
dwayenbo <dwayenbo@student.42.fr>
2025-03-05 09:10:03 +0100

    services started ok

--- backend/auth/entrypoint.sh
+++ backend/auth/entrypoint.sh
@@ -75,6 +75,34 @@ until pg_isready -d $POSTGRES_DB -h db -p 5432 -U $AUTH_USER >/dev/null; do
 	echo "Connexion to database didn't succeed, retrying..."
 	sleep 2
 done
+
+wait_for_start() {
+	retries=0
+	max_retries=12
+	ip=""
+	while [ x"$ip" = x ] && [ $retries -lt $max_retries ]; do
+		ip=$(getent hosts "$1" | awk '{print $1}')
+		if [ x"$ip" = x ]; then
+			echo "Waiting for $1 to resolve... attempt $((retries + 1))"
+			sleep 5
+			retries=$((retries + 1))
+		fi
+	done
+
+	if [ x"$ip" = x ]; then
+		echo "Error: Failed to resolve $1 after 60 seconds. Exiting."
+		exit 1
+	fi
+	echo ${1^} container has successfully started.
+}
+
+read -r -d '' containers <<'EOF'
+postgres
+logstash
+EOF
+for container in $containers; do
+	wait_for_start $container
+done
 echo "🚀 Environment variables were properly set using Vault, launching $service"
 
 exec "$@"
--- devops/nginx_exporter/entrypoint.sh
+++ devops/nginx_exporter/entrypoint.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
+wait_for_start() {
+    retries=0
+    max_retries=12
+    ip=""
+    while [ x"$ip" = x ] && [ $retries -lt $max_retries ]; do
+        ip=$(getent hosts "$1" | awk '{print $1}')
+        if [ x"$ip" = x ]; then
+            echo "Waiting for $1 to resolve... attempt $((retries + 1))"
+            sleep 5
+            retries=$((retries + 1))
+        fi
+    done
 
+    if [ x"$ip" = x ]; then
+        echo "Error: Failed to resolve $1 after 60 seconds. Exiting."
+        exit 1
+    fi
+    echo ${1^} container has successfully started.
+}
+
+read -r -d '' containers <<'EOF'
+nginx
+prometheus
+logstash
+EOF
+for container in $containers; do
+    wait_for_start $container
+done
 rsyslogd -i /tmp/rsyslogd.pid -f /syslog/rsyslog.conf
 /logrotate_script.sh &
 exec "$@" >>/tmp/log/nginx_exporter.log 2>&1
