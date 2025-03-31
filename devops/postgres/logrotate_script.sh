00dff3fa7f85fdc09740530d44f7f15e0261e3a3
dwayenbo <dwayenbo@student.42.fr>
2025-03-31 20:25:33 +0200

    changement timer logrotate

--- devops/postgres/logrotate_script.sh
+++ devops/postgres/logrotate_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-    logrotate -f -s /tmp/logrotate.status /etc/logrotate.conf
+    logrotate -s /tmp/logrotate.status /etc/logrotate.conf
     sleep 86400 # wait 1 day
 done
--- nginx/logrotate_script.sh
+++ nginx/logrotate_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true; do
-    logrotate -f -s /tmp/logrotate.status /etc/logrotate.conf
-    sleep 300 # wait 5 minutes
+    logrotate -s /tmp/logrotate.status /etc/logrotate.conf
+    sleep 86400 # wait 1 day
 done
