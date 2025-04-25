c04f650ab43b71f5071148a18744621ea165ccc7
dwayenbo <dwayenbo@42.student.fr>
2025-04-25 14:13:16 +0200

    manquait des variables d'env

--- backend/auth/files/auth_project/settings.py
+++ backend/auth/files/auth_project/settings.py
@@ -224,7 +224,7 @@ STATIC_URL="http://localhost:8000/static/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKED = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
